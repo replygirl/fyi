c4706ee6b899a288dafb089331218c36d31b7583
imogen <im@imogen.fyi>
2018-03-02 18:04:42 -0800

    form

--- source/js/site.js
+++ source/js/site.js
@@ -10,6 +10,20 @@ angular.module('fyi', [])
     fyi.activeIfContact = (s) => fyi.contact == s ? 'active':''
     fyi.openContact = () => fyi.contact = 'open'
     fyi.sendContact = () => {
-      fyi.contact = 'success'
+      fetch('https://api.clockworksms.com/http/send.aspx', {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify({
+          'key': '43b5fe9ff7818faadfae43ecaa0cab76be5ac346',
+          'to': 12063989614,
+          'content':
+            `New from ${fyi.message.name} (${fyi.message.email}): ${fyi.message.body}`,
+          'long': 1
+        }),
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
     }
   });
