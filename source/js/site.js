a67acdef9c933cca7e31dd06e4fe6e560eb0576f
imogen <im@imogen.fyi>
2018-03-02 17:05:51 -0800

    scaffold contact

--- source/js/site.js
+++ source/js/site.js
@@ -2,7 +2,14 @@ angular.module('fyi', [])
   .controller('fyiController', ($scope) => {
     let fyi = $scope
     fyi.state = 'design.product'
+    fyi.contact = false
 
     fyi.to = (s) => fyi.state = s
-    fyi.activeIf = (s) => fyi.state.includes(s) ? 'active' : ''
+    fyi.activeIf = (s) => fyi.state.includes(s) ? 'active':''
+
+    fyi.activeIfContact = (s) => fyi.contact == s ? 'active':''
+    fyi.openContact = () => fyi.contact = 'open'
+    fyi.sendContact = () => {
+      fyi.contact = 'success'
+    }
   });
