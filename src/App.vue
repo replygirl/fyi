a727a425f93c519606cf002f4cd1f74ba07d41bd
im <im@imogen.fyi>
2018-07-09 20:36:56 -0400

    styles
    oops

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,16 @@
     span.title= title + '. '
     if block
       block
-    | &#32;
-
+      | &#32;
   #app
     +section('imogen')
       Copyable(label='email' value='im@imogen.fyi')
     +section('music')
       | sound design. arrangement. engineering.&#32;
-      a(href='soundcloud.com/imogennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn') soundcloud
+      a(href='//soundcloud.com/imogennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn') soundcloud
       | .&#32;
     +section('tech')
-      | pm. design. dev. medical provider directory for queer people. campus management & geospatial analytics for schools. low-bandwidth messaging for support teams. program & professor directory for science & engineering students. ai-based canvassing for grassroots campaigns. if your mother could change the world, what would she change? bespoke cms for a national newsroom. reporting, dispatch, & content sales for amateur photojournalists & broadcast newsrooms. personal cms for art dealers & collectors. social restaurant reviews for picky eaters. did you know americans underspend on food relative to their income? markdown editor with syntax extensions for technical writers. morse code sms for the vision- and motor-impaired. restaurant recommendation app for the indecisive. robots that play fake sports.
+      | pm. design. dev. medical provider directory for queer people. campus management & geospatial analytics for schools. low-bandwidth messaging for support teams. program & professor directory for science & engineering students. ai-based canvassing for grassroots campaigns. if your mother could change the world, what would she change? bespoke cms for a national newsroom. reporting, dispatch, & content sales for amateur photojournalists & broadcast newsrooms. personal cms for art dealers & collectors. social restaurant reviews for picky eaters. did you know americans underspend on food relative to their income? markdown editor with syntax extensions for technical writers. morse code sms for the vision- and motor-impaired.
 </template>
 
 <script>
@@ -31,13 +30,16 @@ export default {
 *
   box-sizing border-box
 
+html
+  background-color yellow
+
 html, body
   width 100%
   height 100%
-  margin 0
-  padding 0
   overflow hidden
-  background-color black
+
+body
+  margin 0
 
 #app
   --line-height 28px
@@ -45,7 +47,6 @@ html, body
   font-weight 300
   font-size 24px
   line-height var(--line-height)
-  color white
 
   height: calc(100% + calc(2 * var(--line-height)))
   max-width 640px
@@ -56,7 +57,7 @@ html, body
   overflow-y scroll
 
 .title
-  color yellow
+  color magenta
 
 a
   &, &:link, &:visited, &:hover, &:active
