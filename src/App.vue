94bcb2d84d3bcf1653fa10795fd07d9b7bf8a2d3
im <im@imogen.fyi>
2018-07-18 22:01:35 -0400

    add ecommerce sticker

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template lang='pug'>
-  mixin section(title)
-    span.title= title + '. '
-    if block
-      block
-      | &#32;
   #app
-    +section('imogen')
-      Copyable(label='email' value='im@imogen.fyi')
-    +section('music')
-      | sound design. arrangement. engineering.&#32;
-      a(href='//soundcloud.com/imogennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn') soundcloud
-      | .&#32;
-    +section('tech')
-      | RRRRRRRRRRRRRRRRpm. design. dev. medical provider directory for queer people. campus management & geospatial analytics for schools. low-bandwidth messaging for support teams. program & professor directory for science & engineering students. ai-based canvassing for grassroots campaigns. bespoke cms for a national newsroom. reporting, dispatch, & content sales for amateur photojournalists & broadcast newsrooms. personal cms for art dealers & collectors. social restaurant reviews for picky eaters. markdown editor with syntax extensions for technical writers. morse code sms for the vision- and motor-impaired.
+    Body
+    a#ecommerce.hide-text(href='//gumroad.com/l/MWThS') badass ecommerce shirt
 </template>
 
 <script>
-import Copyable from './components/Copyable.vue'
-import heap     from './vendor/heap.js'
+import Body from './components/Body.vue'
+import heap from './vendor/heap.js'
 
 export default {
   name: 'app',
   components: {
-    Copyable
+    Body
   },
   mounted () {
     heap()
@@ -49,30 +38,13 @@ path = '../public/webfonts/'
 html
   background-color yellow
 
-html, body
+html, body, #app
   width 100%
   height 100%
   overflow hidden
 
 body
   margin 0
-
-#app
-  --line-height 28px
-  --two-line-height calc(2 * var(--line-height))
-
-  font-family 'BasicCommercialLTCom-Bold', Helvetica, Arial, sans-serif
-  font-weight bold
-  font-size 24px
-  line-height var(--line-height)
-
-  height: calc(100% + var(--two-line-height))
-  max-width 640px
-  margin-top calc(-1 * var(--line-height))
-  padding var(--line-height) var(--two-line-height) var(--two-line-height) 0
-  transform-origin right top
-  transform rotate(-2.5deg)
-  overflow-y scroll
 
 .title
   color magenta
@@ -82,4 +54,28 @@ a
     text-decoration underline
     color inherit
     cursor pointer
+
+.hide-text
+  line-height 0
+  font-size 0
+  color transparent
+
+#ecommerce
+  position fixed
+  bottom 56px
+  right -6px
+  transform-origin right center
+  transform rotate(1.5deg)
+  width 168px
+  height 81px
+  background-image url('../public/img/ecommerce.svg')
+  background-size contain
+  @media (min-width: 480px)
+    right -10px
+    width 252px
+    height 121px
+  @media (min-width: 1024px)
+    right -12px
+    width 504px
+    height 242px
 </style>
---
+++ src/components/Body.vue
@@ -0,0 +1,48 @@
+<template lang='pug'>
+  mixin section(title)
+    span.title= title + '. '
+    if block
+      block
+      | &#32;
+  #body
+    +section('imogen')
+      Copyable(label='email' value='im@imogen.fyi')
+    +section('music')
+      | sound design. arrangement. engineering.&#32;
+      a(href='//soundcloud.com/imogennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn') soundcloud
+      | .&#32;
+    +section('tech')
+      | pm. design. dev. medical provider directory for queer people. campus management & geospatial analytics for schools. low-bandwidth messaging for support teams. program & professor directory for science & engineering students. ai-based canvassing for grassroots campaigns. bespoke cms for a national newsroom. reporting, dispatch, & content sales for amateur photojournalists & broadcast newsrooms. personal cms for art dealers & collectors. social restaurant reviews for picky eaters. markdown editor with syntax extensions for technical writers. morse code sms for the vision- and motor-impaired.
+</template>
+
+<script>
+import Copyable from './Copyable.vue'
+
+export default {
+  name: 'Body',
+  components: {
+    Copyable
+  }
+}
+</script>
+
+<style lang="stylus" scoped="true">
+#body
+  --line-height 28px
+  --two-line-height calc(2 * var(--line-height))
+
+  font-family 'BasicCommercialLTCom-Bold', Helvetica, Arial, sans-serif
+  font-weight bold
+  font-size 24px
+  line-height var(--line-height)
+
+  height: calc(100% + calc(2 * var(--line-height)))
+  max-width 640px
+  margin-top calc(-1 * var(--line-height))
+  padding var(--line-height) var(--two-line-height) calc(112px + var(--two-line-height)) 0
+  @media (min-width: 960px)
+    padding-bottom var(--two-line-height)
+  transform-origin right top
+  transform rotate(-2.5deg)
+  overflow-y scroll
+</style>
