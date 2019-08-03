993d1c6e42f555f6d322dfb3573bc2145a7cf344
moon <moon@moon.fyi>
2019-08-03 19:09:28 -0400

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template lang='pug'>
   #app
+    header
+      h1 Moon
+      p Process & product manager
+    main
+    footer
+      a(:href='soundcloud') I also make music
+      p moon (at) moon (dot) fyi
 </template>
 
 <script>
@@ -8,6 +15,11 @@ import 'reset-css'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      soundcloud: 'https://soundcloud.com/halfsister'
+    }
+  },
   mounted() {
     heap(process.env.VUE_APP_HEAP_ID)
   }
@@ -17,22 +29,76 @@ export default {
 <style lang="stylus">
 @import url('//api.fontstore.com/webfonts/53b2ac8b-d49b-452a-812a-68efd57593aa')
 
+button()
+  --color black
+  display inline-block
+  position relative
+  margin 0 -10px
+  padding 0 10px
+  line-height 30px
+  height @line-height
+  border 0.5px solid var(--color)
+  box-shadow 2px 2px var(--color)
+  background-color white
+  color var(--color)
+  text-decoration none
+  transition all 100ms ease-in
+  @media (hover: hover)
+    &:hover
+      transform translateX(1px)
+      box-shadow 0px 0px var(--color)
+
 *
   box-sizing border-box
 
-html
-  --font-size 34px
-  --line-height 40px
-  --background-color white
-  --color black
+:root, body, #app
+  width 100%
+  height 100%
+
+:root
+  --color-primary #dfeaea
+  --color-secondary #eae0df
   font-family 'Clash Moon', Helvetica, Arial, sans-serif
   -webkit-text-size-adjust 100%
-  font-size var(--font-size)
-  line-height var(--line-height)
-  background-color var(--background-color)
-  color var(--color)
+  font-size 15px
+  line-height 10px
+  background-color white
+  color black
 
-html, body, #app
-  width 100%
-  height @width
+#app
+  display flex
+  flex-direction column
+
+main
+  flex 1
+  background-color var(--color-secondary)
+
+header, footer
+  padding 20px
+  background-image embedurl('assets/zigzag.svg', 'utf8')
+  > *:not(:first-child)
+    margin-top 10px
+  h1, p
+    display block
+    margin 0 -20px
+    padding 0 20px
+    background-color var(--color-primary)
+  h1
+    font-size 30px
+    line-height 20px
+    height @line-height
+  p
+    height 10px
+
+header
+  padding-bottom 40px
+
+footer
+  padding-top 40px
+  display flex
+  flex-direction column
+  a
+    button()
+    --color #f30
+    align-self flex-end
 </style>
