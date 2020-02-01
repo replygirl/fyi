9fc931051550ace86fce91c5a69503953eed1884
moon <moon@moon.fyi>
2020-02-01 15:14:55 -0500

    seed content

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,44 @@
 <template lang='pug'>
   #app
+    header
+      section
+        h1 Imogen Moon
+        | she/they
+      section
+        span moon at moon dot fyi
+        a(href='https://soundcloud.com/halfsister') soundcloud.com/halfsister
+    main
+      section
+        ul
+          li copy a
+          li copy b
+          li copy c
+      section
+        h6 Past
+        ul
+          li
+            | role a
+            a(href='') team a
+          li
+            | role b
+            a(href='') team b
+          li
+            | role c
+            a(href='') team c
+      section
+        h6 Present
+        ul
+          li
+            | role a
+            a(href='') team a
+          li
+            | role b
+            a(href='') team b
+          li
+            | role c
+            a(href='') team c
+          li thing d
+          li thing e
 </template>
 
 <script>
