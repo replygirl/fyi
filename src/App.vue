053e538d914583457cb15885c63f07859fcb250d
moon <moon@moon.fyi>
2020-02-01 15:02:26 -0500

    add plex

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,10 @@ export default {
   name: 'app'
 }
 </script>
+
+<style lang='stylus'>
+@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i,700&display=swap')
+
+:root
+  font-family 'IBM Plex Mono', monospace
+</style>
